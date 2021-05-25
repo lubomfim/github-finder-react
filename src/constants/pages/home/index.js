20362b5bcc606f3f2d9ebd1754ca46c59da71847
Lucas Bomfim <lucasliko@hotmail.com>
2021-05-25 09:00:01 -0300

    feat: static content comes from constants now

---
+++ src/constants/pages/home/index.js
@@ -0,0 +1,7 @@
+export const homeData = {
+  title: 'GitHub Finder',
+  text: 'You can search and discover lots of things about github users!',
+  placeholder: 'Type the username ( e.g lubomfim)',
+  icon: 'images/icons/search.svg',
+  alt: 'Search Icon',
+};
