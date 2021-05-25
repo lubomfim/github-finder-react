b3bf6655a5a28c65f6d87aaa989d7f61c7ba9293
Lucas Bomfim <lucasliko@hotmail.com>
2021-05-25 08:58:32 -0300

    refactor: removing containers and merging into pages. Static content come from constants

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,61 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useLazyQuery } from '@apollo/client';
+import { useHistory } from 'react-router';
+import { USER } from 'services/graphql/queries';
 
 import { Wrapper } from 'components/Wrapper';
-import { SearchUser } from 'containers/SearchUser';
-import { HomeHero } from 'containers/HomeHero';
+import { Logo } from 'components/Logo';
+import { Loader } from 'components/Loader';
+import { Warming } from 'components/Warming';
+import { SearchInput } from 'components/SearchInput';
+
+import * as S from './styled';
+import { homeData } from 'constants/pages/home';
 
 function Home() {
+  const [search, setSearch] = useState('');
+  const [runQuery, { called, loading, data, error }] = useLazyQuery(USER);
+  const history = useHistory();
+
+  const handleChange = useCallback((e) => {
+    setSearch(e);
+  }, []);
+
+  const handleClick = useCallback(() => {
+    runQuery({
+      variables: {
+        login: search,
+      },
+      onError: (err) => console.log(err.message),
+    });
+  }, [runQuery, search]);
+
+  useEffect(() => {
+    if (data?.user && called && !loading) {
+      history.push(`user/${search}`);
+    }
+  }, [data, called, loading, history, search]);
+
   return (
     <Wrapper>
-      <HomeHero text="You can search and discover lots of things about github users!" title="GitHub Finder" />
-      <SearchUser />
+      <Logo $home={true} />
+      <S.HomeHeroWrapper>
+        <S.HomeHeroTitle>{homeData.title}</S.HomeHeroTitle>
+        <S.HomeHeroText>{homeData.text}</S.HomeHeroText>
+      </S.HomeHeroWrapper>
+      <S.SearchWrapper onKeyPress={(e) => (e.code === 'Enter' ? handleClick() : null)}>
+        <SearchInput
+          placeholder={homeData.placeholder}
+          onChange={(e) => handleChange(e.target.value)}
+          value={search}
+          disabled={loading ? true : false}
+        />
+        <S.SearchButton onClick={() => handleClick()} disabled={loading ? true : false}>
+          <S.SearchIcon src={homeData.icon} alt={homeData.alt} />
+        </S.SearchButton>
+      </S.SearchWrapper>
+      {loading && <Loader />}
+      {error && <Warming type={error.graphQLErrors[0].type} />}
     </Wrapper>
   );
 }
--- src/pages/Home/styled.js
+++ src/pages/Home/styled.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 import * as C from '../../styles/colors';
+import * as T from 'styles/typography';
 
 export const Title = styled.h1`
   color: ${C.WHITE};
@@ -9,3 +10,52 @@ export const Title = styled.h1`
     color: blue;
   }
 `;
+
+export const HomeHeroWrapper = styled.div`
+  text-align: center;
+  margin: 140px 0px 45px;
+`;
+
+export const HomeHeroTitle = styled.h1`
+  ${T.TITLE_EXTRA_LARGE};
+  margin-bottom: 20px;
+`;
+
+export const HomeHeroText = styled.p`
+  ${T.BODY_TEXT_LARGE_BOLD};
+  color: ${C.SECONDARY_COLOR};
+  font-family: ${T.SECONDARY_FONT};
+
+  @media (max-width: 600px) {
+    width: 90%;
+    margin: 0 auto;
+  }
+`;
+
+export const SearchWrapper = styled.form`
+  display: flex;
+  margin: 0 auto 30px;
+  max-width: 500px;
+  width: 90%;
+`;
+
+export const SearchButton = styled.button`
+  background-color: ${C.BLACK};
+  width: 80px;
+  height: 63px;
+  position: relative;
+  bottom: -3px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  border: 1px solid ${C.BLACK};
+
+  &:hover {
+    width: 90px;
+  }
+
+  &:active {
+    box-shadow: inset 3px 3px ${C.WHITE};
+  }
+`;
+
+export const SearchIcon = styled.img``;
