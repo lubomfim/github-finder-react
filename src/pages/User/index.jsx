c3a03307724b027960f3fc797e4af3456f89cf57
Lucas Bomfim <lucasliko@hotmail.com>
2021-06-02 11:46:59 -0300

    refactor: separated pages components to modules

--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -1,49 +1,20 @@
-import React, { useState } from 'react';
-import { USER_PROFILE } from 'services/graphql/queries';
-
-import { Wrapper } from 'components/Wrapper';
-import { Loader } from 'components/Loader';
-import { useQuery } from '@apollo/client';
+import React from 'react';
 import { useParams } from 'react-router';
 
+import { Wrapper } from 'components/Wrapper';
+import UserProfile from 'modules/UserProfile';
+import UserRepositories from 'modules/UserRepositories';
+
 import * as S from './styled';
-import { Logo } from 'components/Logo';
 
 const User = () => {
   const { user } = useParams();
-  const [userProfile, setUserProfile] = useState({});
-  const { loading, error, data } = useQuery(USER_PROFILE, {
-    variables: {
-      login: user,
-    },
-    onCompleted: () => setUserProfile(data.user),
-  });
   return (
     <Wrapper>
-      {loading && <Loader />}
-      {
-        <S.UserWrapper>
-          <S.UserProfile>
-            <S.UserProfileImgWrapper>
-              <S.UserProfileImg src={userProfile.avatarUrl} alt={userProfile.login} />
-            </S.UserProfileImgWrapper>
-            <S.UserProfileName>{userProfile.name}</S.UserProfileName>
-            <S.UserProfileLogin href={userProfile.url} target="blank">
-              @{userProfile.login}
-            </S.UserProfileLogin>
-            <S.UserProfileBio>{userProfile.bio}</S.UserProfileBio>
-            <S.UserProfileNumber>{userProfile.followers?.totalCount} Followers</S.UserProfileNumber>
-            <S.UserProfileNumber>{userProfile.following?.totalCount} Following</S.UserProfileNumber>
-            <S.UserProfileNumber>{userProfile.starredRepositories?.totalCount} Stars</S.UserProfileNumber>
-
-            <S.GoBackButton to="/"> Search a new user</S.GoBackButton>
-          </S.UserProfile>
-          <S.UserRepositories>
-            <Logo />
-            <S.UserRepositoriesList></S.UserRepositoriesList>
-          </S.UserRepositories>
-        </S.UserWrapper>
-      }
+      <S.UserWrapper>
+        <UserProfile login={user} />
+        <UserRepositories login={user} />
+      </S.UserWrapper>
     </Wrapper>
   );
 };
